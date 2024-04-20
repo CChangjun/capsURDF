7b6239f7502e6af2d4e5685623ac43f766bd73ab
changjun <ihlab@changjun.mshome.net>
2024-04-20 17:12:22 +0900

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ihlab/robot_four/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ihlab/catkin_ws_car/install_isolated;/home/ihlab/catkin_ws/devel;/home/ihlab/catkin_ws/install_isolated;/opt/ros/noetic'.split(';') if '/home/ihlab/catkin_ws_car/install_isolated;/home/ihlab/catkin_ws/devel;/home/ihlab/catkin_ws/install_isolated;/opt/ros/noetic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ihlab/robot_four/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ihlab/robot_four/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/ihlab/robot_four/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/ihlab/robot_four/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ihlab/robot_four/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/ihlab/robot_four/src:$ROS_PACKAGE_PATH"
